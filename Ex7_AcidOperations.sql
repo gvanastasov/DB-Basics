4375f677408cd4d89ae4231b75dae6eae9977177
gva <gvanastasov@abv.bg>
2017-02-18 22:59:29 +0100

    task 4 done

--- Ex7_AcidOperations.sql
+++ Ex7_AcidOperations.sql
@@ -9,22 +9,49 @@ AS
 	 where [Salary] > 35000
 
 exec dbo.usp_GetEmployeesSalaryAbove35000
-
+go
 -- 02. Employees with Salary Above Number
 
+create procedure usp_GetEmployeesSalaryAboveNumber
+	@tresshold money
+as
+	select [FirstName], [LastName] FROM Employees
+	 where [Salary] >= @tresshold
 
+exec dbo.usp_GetEmployeesSalaryAboveNumber @tresshold = 48100
+go
 
+-- 03 Town Names Starting With
 
+create procedure usp_GetTownsStartingWith
+	@startChar nvarchar(max)
+as
+	select [Name] from Towns
+	 where Left([Name], LEN(@startChar)) = @startChar
 
+exec dbo.usp_GetTownsStartingWith @startChar = 'bo'
+go
 
+-- 04. Emplpoyees from Town
 
+create procedure usp_GetEmployeesFromTown
+	@townName nvarchar(max)
+as
+	select [FirstName], [LastName] 
+	from 
+		Employees as e
 
+		inner join (
+			select a.[AddressID], t.[Name] from Addresses as a
+			inner join Towns as t on a.[TownID]= t.[TownID]
+		) as atn on e.[AddressID] = atn.[AddressID]
 
+	where atn.[Name] = @townName 
 
+exec dbo.usp_GetEmployeesFromTown @townName = 'Sofia'
+go
 
-
-
-
+--
 
 
 
