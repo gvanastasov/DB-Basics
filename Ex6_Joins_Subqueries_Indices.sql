9ef2bf2ac896a50a5df4fb1309b4f7fd2e02df0c
gva <gvanastasov@abv.bg>
2017-02-18 10:22:44 +0100

    task 2 done

--- Ex6_Joins_Subqueries_Indices.sql
+++ Ex6_Joins_Subqueries_Indices.sql
@@ -11,7 +11,16 @@ from Employees as e
 JOIN Addresses as a ON e.[AddressID] = a.[AddressID]
 ORDER BY e.[AddressID]
 
+-- 02. Addresses with Towns
 
+select TOP(50) [FirstName], 
+				[LastName], 
+				t.[Name] as [Town], 
+				a.[AddressText]
+from Employees as e
+JOIN Addresses as a ON e.[AddressID] = a.[AddressID]
+JOIN Towns as t ON a.[TownID] = t.[TownID]
+ORDER BY [FirstName] asc, [LastName] desc
 
 
 
