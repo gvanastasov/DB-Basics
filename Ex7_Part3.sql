dc899b04e9d91e0ef2c91c39122246a690399379
gva <gvanastasov@abv.bg>
2017-02-19 07:02:46 +0100

    task 20 done

--- Ex7_Part3.sql
+++ Ex7_Part3.sql
@@ -44,6 +44,68 @@ as
 go
 
 select * from dbo.ufn_CashInUsersGames('Lily Stargazer')
+go
+
+-- 20. Trigger
+
+create trigger tr_OnUserAddItem on UserGameItems instead of INSERT
+as
+	begin
+		
+		declare @itemId int, 
+				@userGameId int,
+				@userLevel int,
+				@itemLevel int,
+				@itemPrice money;
+		
+		select @itemId = [ItemId], 
+			   @userGameId = [UserGameId] 
+		from inserted
+
+		select @userLevel = [Level]
+		from UsersGames
+		where [Id] = @userGameId
+
+		select @itemLevel = [MinLevel],
+				@itemPrice = [Price]
+		from Items
+		where [Id] = @itemId
+
+		declare @t nvarchar(200) = Concat('ugameid: ',@userGameId,'ulvl: ',@userLevel,' ilvl: ',@itemLevel);
+		raiserror(@t, 0,0)  
+
+		if @userLevel >= @itemLevel
+			begin
+				insert into UserGameItems
+				values (@itemId, @userGameId)
+
+				update UsersGames set [Cash] -= @itemPrice
+				where [Id] = @userGameId
+
+			end
+		else
+			raiserror('Character level is too low',1,16)
+	end
+go
+
+delete from UserGameItems
+where [ItemId] = 6 and [UserGameId] = 212
+
+-- will not work coz of level trigger
+insert into UserGameItems
+values (6, 212)
+
+
+
+--
+
+
+
+
+
+
+
+
 
 
 
